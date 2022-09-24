a12e8f6292bab1ae49496f0b9b5ad2425461c1b1
icyhot12 <rmierniczek1@gmail.com>
2022-09-24 18:32:53 +0200

    add switch logic

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+btn.addEventListener('click',() => {
+    if (!btn.classList.contains('slide')) {
+        btn.classList.add('slide')
+    } else {
+        btn.classList.remove('slide')
+    }
+})
