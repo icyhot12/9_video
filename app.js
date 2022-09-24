c24a847d2b09fe63e64f4209e502a605620f335d
icyhot12 <rmierniczek1@gmail.com>
2022-09-24 18:55:48 +0200

    add preloader

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const video = document.querySelector('.video-container');
 
 btn.addEventListener('click',() => {
     if (!btn.classList.contains('slide')) {
-        btn.classList.add('slide')
+        btn.classList.add('slide');
+        video.pause();
     } else {
-        btn.classList.remove('slide')
+        btn.classList.remove('slide');
+        video.play();
     }
+});
+
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+    preloader.classList.add('hide-preloader')
 })
